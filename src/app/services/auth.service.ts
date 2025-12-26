9919bd1576c51054a000d6d39dc85789af4e2322
viniii05 <vinivt.0520@gmail.com>
2025-12-26 19:55:05 +0000

    login in progress

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,18 +3,60 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BehaviorSubject } from 'rxjs';
 
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AuthService {
+//   private loggedInSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
+//   loggedIn$ = this.loggedInSubject.asObservable();
+//   // private baseUrl = 'https://userservice-tg0w.onrender.com/api/users/register';  // Update base URL
+//   private baseUrl = 'http://localhost:9090/api/users';
+//   constructor(private http: HttpClient) {}
+
+//   private isLoggedIn(): boolean {
+//     return localStorage.getItem('user') !== null; // Assuming user data is saved in localStorage
+//   }
+
+//   login(user: any): void {
+//     localStorage.setItem('user', JSON.stringify(user));
+//     this.loggedInSubject.next(true);
+//   }
+
+//   logout(): void {
+//     localStorage.removeItem('user');
+//     this.loggedInSubject.next(false);
+//   }
+
+//   get isUserLoggedIn(): boolean {
+//     return this.loggedInSubject.getValue();
+//   }
+
+//   signUp(payload: any): Observable<any> {
+//     const url = `${this.baseUrl}/register`;
+
+//     return this.http.post(url, payload, { responseType: 'text' });
+
+//   }
+
+//   verifyOtp(email: string, otp: string): Observable<any> {
+//   return this.http.post(`${this.baseUrl}/verify-otp`, { email, otp }, { responseType: 'text' });
+// }
+
+// }
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private loggedInSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
   loggedIn$ = this.loggedInSubject.asObservable();
-  // private baseUrl = 'https://userservice-tg0w.onrender.com/api/users/register';  // Update base URL
   private baseUrl = 'http://localhost:9090/api/users';
+
   constructor(private http: HttpClient) {}
 
   private isLoggedIn(): boolean {
-    return localStorage.getItem('user') !== null; // Assuming user data is saved in localStorage
+    return localStorage.getItem('user') !== null;
   }
 
   login(user: any): void {
@@ -33,15 +75,15 @@ export class AuthService {
 
   signUp(payload: any): Observable<any> {
     const url = `${this.baseUrl}/register`;
-
     return this.http.post(url, payload, { responseType: 'text' });
-
   }
 
   verifyOtp(email: string, otp: string): Observable<any> {
-  return this.http.post(`${this.baseUrl}/verify-otp`, { email, otp }, { responseType: 'text' });
+    return this.http.post(`${this.baseUrl}/verify-otp`, { email, otp }, { responseType: 'text' });
+  }
+
+  loginWithCredentials(credentials: any): Observable<any> {
+    const url = `${this.baseUrl}/login`;
+    return this.http.post(url, credentials, { responseType: 'text' });
+  }
 }
-
-}
-
-
